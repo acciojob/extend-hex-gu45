49daba2a4b1c1043b4d0e24b00d7dfd2cc36cbb0
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-11 20:06:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -15,5 +15,5 @@ const extendHex = (shortHex) => {
 };
 
 // Do not change the code below.
-// const shortHex = prompt("Enter Short Hex.");
+const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
