a8db3b8f4c93ea3dca79b705caf184b44b928a80
gu45 <86247735+gu45@users.noreply.github.com>
2023-12-11 20:06:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length-3;
+    let res = "#"
+    let isupper = false
+    while(i<shortHex.length){
+        let aciicode = shortHex[i].charCodeAt(0)
+        if(aciicode>=65 && aciicode<=90){
+            isupper = true;
+        }
+        res = res+shortHex[i]+shortHex[i]
+        i++;
+    }
+    return isupper ? res.toUpperCase() : res;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
+// const shortHex = prompt("Enter Short Hex.");
 alert(extendHex(shortHex));
